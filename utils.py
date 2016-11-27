ef3d3b89688b524ff3deec173000650b277014a5
Daniel Ribeiro <djbreves@gmail.com>
2016-11-27 19:51:52 +1100

    Flush print

--- utils.py
+++ utils.py
@@ -2,4 +2,4 @@ HEADER_COLOR = '\033[95m'
 END_COLOR = '\033[0m'
 
 def print_result(result):
-    print('\n', HEADER_COLOR + result + END_COLOR, '\n--------------------')
+    print('\n', HEADER_COLOR + result + END_COLOR, '\n--------------------', flush=True)
